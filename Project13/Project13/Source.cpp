07c9aa70c800905038576b47c25fe49a5e446a75
Clara <rustiktie@gmail.com>
2017-03-02 10:43:21 +0100

    Update 0.1
    Quit when hit Escape

--- Project13/Project13/Source.cpp
+++ Project13/Project13/Source.cpp
@@ -16,22 +16,35 @@ int main(int argc, char* argv[]) {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 
-	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer); //all red with a box in the middle blue
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-		
-	SDL_Rect rectangle{ 160, 120, 320, 240 };
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-	SDL_RenderDrawRect(renderer, &rectangle);
-	
-	SDL_RenderFillRect(renderer, &rectangle);
-	SDL_RenderPresent(renderer);
 
+	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer); //SCREEN SIZE
 
-	for (;;) 
+	bool quit = false;
+
+	while(quit == false) //SCREEN COLORS AND RECTANGLE
 	{
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderClear(renderer);
 
+		SDL_Rect rectangle{ 160, 120, 320, 240 };
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderDrawRect(renderer, &rectangle);
+
+		SDL_RenderFillRect(renderer, &rectangle);
+		SDL_RenderPresent(renderer);
+
+		SDL_Event event; 
+
+		while (SDL_PollEvent(&event)) //QUIT WHEN HIT ESCAPE
+		{
+
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				quit = true;
+			}
+
+		}
 	}
+
 
 	SDL_Quit();
 
