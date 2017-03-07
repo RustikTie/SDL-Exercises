b0332214e09722a0987648306f36d766f1ca6927
Clara <rustiktie@gmail.com>
2017-03-07 23:04:36 +0100

    Added moving background / Bullets still not OK

--- Project13/Project13/Source.cpp
+++ Project13/Project13/Source.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib, "SDL/lib.x86/SDL2.lib")
 #pragma comment(lib, "SDL/lib.x86/SDL2main.lib")
 #define MAX_BULLETS 2
+#define MAX_BACKGROUND 2
 
 
 int main(int argc, char* argv[]) {
@@ -18,27 +19,28 @@ int main(int argc, char* argv[]) {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Surface* todd_surface;
-	SDL_Surface* standby_surface;
+	SDL_Surface* bullshit_surface;
 	SDL_Surface* lies_surface;
-	SDL_Texture* standby_texture;
+	SDL_Texture* bullshit_texture;
 	SDL_Texture* todd_texture;
 	SDL_Texture* lies_texture;
 	SDL_Rect rectangle{ 0, 240, 123, 158 };	
-	SDL_Rect background{ 0 ,0, 640, 480};
-	
-	SDL_Rect bullet[MAX_BULLETS] = { -58, -19, 58, 19 };
-
+	SDL_Rect background_1 = { 0 ,0, 1024, 512};
+	SDL_Rect background_2 = { 1024, 0, 1024, 512 };
+	SDL_Rect bullet[MAX_BULLETS]; // = { -58, -19, 58, 19 };	
 
 	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer); //SCREEN SIZE
 
+	////////////////////////////TEXTURES AND SURFACES////////////////////////////
 	todd_surface = SDL_LoadBMP("Todd.bmp");
-	standby_surface = SDL_LoadBMP("standby.bmp");
+//	standby_surface = SDL_LoadBMP("standby.bmp");
+	bullshit_surface = SDL_LoadBMP("bullshit.bmp");
 	lies_surface = SDL_LoadBMP("lies.bmp");
 	todd_texture = SDL_CreateTextureFromSurface(renderer, todd_surface);
-	standby_texture = SDL_CreateTextureFromSurface(renderer, standby_surface);
+	bullshit_texture = SDL_CreateTextureFromSurface(renderer, bullshit_surface);
 	lies_texture = SDL_CreateTextureFromSurface(renderer, lies_surface);
    	SDL_FreeSurface(todd_surface);
-	SDL_FreeSurface(standby_surface);
+	SDL_FreeSurface(bullshit_surface);
 	SDL_FreeSurface(lies_surface);
 
 	bool quit = false;
@@ -48,16 +50,17 @@ int main(int argc, char* argv[]) {
 
 	while(quit == false) //SCREEN COLORS AND RECTANGLE
 	{
+		/////////////////RENDERING////////////////////////
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, standby_texture, NULL, &background);
+		SDL_RenderCopy(renderer, bullshit_texture, NULL, &background_1);
+		SDL_RenderCopy(renderer, bullshit_texture, NULL, &background_2);
 		SDL_RenderCopy(renderer, todd_texture, NULL, &rectangle);		
 		for (i = 0; i < MAX_BULLETS; ++i) {
 			SDL_RenderCopy(renderer, lies_texture, NULL, &bullet[i]);		
 			bullet[i].x++;
-
-		}		
-
+		}	
+	
 		SDL_Event event;
 
 		if (shoot) {
@@ -66,6 +69,21 @@ int main(int argc, char* argv[]) {
 			bullet[counter].y = rectangle.y + 120;
 			shoot = false;
 		}
+
+		//////////////BACKGROUND MOVEMENT///////////////////////////
+
+		background_1.x--;
+		background_2.x--;
+
+		if (background_1.x == -1024)
+		{
+			background_1.x = 1024;
+		}
+		if (background_2.x == -1024)
+		{
+			background_2.x = 1024;
+		}
+
 
 		///////////////////Events/////////////////////
 		while (SDL_PollEvent(&event)) 
