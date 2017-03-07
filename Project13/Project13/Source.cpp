880cc64fa5ae16588d36ceefd580ad984e5c245b
Clara <rustiktie@gmail.com>
2017-03-07 12:26:46 +0100

    Added bullets - needs tweaking

--- Project13/Project13/Source.cpp
+++ Project13/Project13/Source.cpp
@@ -4,41 +4,73 @@
 
 #pragma comment(lib, "SDL/lib.x86/SDL2.lib")
 #pragma comment(lib, "SDL/lib.x86/SDL2main.lib")
+#define MAX_BULLETS 2
 
 
 int main(int argc, char* argv[]) {
 
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_RENDERER_PRESENTVSYNC;
 
 	//int SDL_CreateWindowAndRenderer(int width, int height, Uint32 window_flags, SDL_Window** window, SDL_Renderer** renderer);
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Surface* todd_surface;
-	SDL_Surface* 
+	SDL_Surface* standby_surface;
+	SDL_Surface* lies_surface;
+	SDL_Texture* standby_texture;
 	SDL_Texture* todd_texture;
-	SDL_Rect rectangle{ 196, 81, 247, 317 };	
-
+	SDL_Texture* lies_texture;
+	SDL_Rect rectangle{ 0, 240, 123, 158 };	
+	SDL_Rect background{ 0 ,0, 640, 480};
 	
+	SDL_Rect bullet[MAX_BULLETS] = { -58, -19, 58, 19 };
+
+
 	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer); //SCREEN SIZE
 
 	todd_surface = SDL_LoadBMP("Todd.bmp");
+	standby_surface = SDL_LoadBMP("standby.bmp");
+	lies_surface = SDL_LoadBMP("lies.bmp");
 	todd_texture = SDL_CreateTextureFromSurface(renderer, todd_surface);
-	SDL_FreeSurface(todd_surface);
+	standby_texture = SDL_CreateTextureFromSurface(renderer, standby_surface);
+	lies_texture = SDL_CreateTextureFromSurface(renderer, lies_surface);
+   	SDL_FreeSurface(todd_surface);
+	SDL_FreeSurface(standby_surface);
+	SDL_FreeSurface(lies_surface);
+
 	bool quit = false;
+	bool shoot = false;
+	int i = 0;
+	int counter = 0;
 
 	while(quit == false) //SCREEN COLORS AND RECTANGLE
 	{
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, standby_texture, NULL, &background);
+		SDL_RenderCopy(renderer, todd_texture, NULL, &rectangle);		
+		for (i = 0; i < MAX_BULLETS; ++i) {
+			SDL_RenderCopy(renderer, lies_texture, NULL, &bullet[i]);		
+			bullet[i].x++;
+
+		}		
+
 		SDL_Event event;
 
-		while (SDL_PollEvent(&event)) 
-		{
-			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-			SDL_RenderClear(renderer);			
-			SDL_RenderCopy(renderer, todd_texture, NULL, &rectangle);
-			SDL_RenderPresent(renderer);
+		if (shoot) {
+			
+			bullet[counter].x = rectangle.x + 50;
+			bullet[counter].y = rectangle.y + 120;
+			shoot = false;
+		}
 
+		///////////////////Events/////////////////////
+		while (SDL_PollEvent(&event)) 
+		{			
+			
 			switch (event.type) {
 				case SDL_KEYDOWN:
 
@@ -47,22 +79,33 @@ int main(int argc, char* argv[]) {
 							quit = true;
 							break;
 						case(SDLK_w):
-							--rectangle.y;							
+							rectangle.y -= 5;							
 							break;
 						case(SDLK_s):
-							++rectangle.y;						
+							rectangle.y += 5;
 							break;
 						case(SDLK_a):
-							--rectangle.x;					
+							rectangle.x -= 5;
 							break;
 						case(SDLK_d):
-							++rectangle.x;
+							rectangle.x += 5;
+							break;
+						case(SDLK_SPACE):
+							++counter;
+							if (counter == MAX_BULLETS) {
+								counter = 0;
+							}							
+							shoot = true;
 							break;
 
 				}
-				break;
+				break;		
 			}
 		}
+		/////////////////Movement
+		SDL_RenderPresent(renderer);
+		SDL_Delay(3);
+		
 	}
 
 	SDL_DestroyTexture(todd_texture);
