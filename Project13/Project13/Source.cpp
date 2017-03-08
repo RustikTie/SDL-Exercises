e7eea627fc8a75eee0b10478ad43907eca41d9e5
Clara <rustiktie@gmail.com>
2017-03-08 14:16:38 +0100

    Todd became a unicorn and he shoots rainbows instead of lies

--- Project13/Project13/Source.cpp
+++ Project13/Project13/Source.cpp
@@ -1,9 +1,11 @@
 #include "SDL/include/SDL.h"
+#include "SDL/include/SDL_image.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 #pragma comment(lib, "SDL/lib.x86/SDL2.lib")
 #pragma comment(lib, "SDL/lib.x86/SDL2main.lib")
+#pragma comment(lib, "SDL/lib.x86/SDL2_image.lib")
 #define MAX_BULLETS 10
 #define MAX_BACKGROUND 2
 
@@ -12,6 +14,7 @@ int main(int argc, char* argv[]) {
 
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_INIT_PNG;
 	SDL_RENDERER_PRESENTVSYNC;
 
 	//int SDL_CreateWindowAndRenderer(int width, int height, Uint32 window_flags, SDL_Window** window, SDL_Renderer** renderer);
@@ -24,7 +27,9 @@ int main(int argc, char* argv[]) {
 	SDL_Texture* bullshit_texture;
 	SDL_Texture* todd_texture;
 	SDL_Texture* lies_texture;
-	SDL_Rect rectangle{ 0, 240, 123, 158 };	
+	SDL_Rect rectangle { 0, 240, 123, 158 };	
+	SDL_Rect unicorn { 0, 0, 200, 120 };
+	SDL_Rect shot{ 0, 0, 350, 80 };
 	SDL_Rect background_1 = { 0 ,0, 1024, 512};
 	SDL_Rect background_2 = { 1024, 0, 1024, 512 };
 	SDL_Rect bullet[MAX_BULLETS]; // = { -58, -19, 58, 19 };	
@@ -38,7 +43,7 @@ int main(int argc, char* argv[]) {
 	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer); //SCREEN SIZE
 
 	//////////////////////TEXTURES AND SURFACES///////////////////////
-	todd_surface = SDL_LoadBMP("Todd.bmp");
+	todd_surface = IMG_Load("unicorn_sprites.png");
 //	standby_surface = SDL_LoadBMP("standby.bmp");
 	bullshit_surface = SDL_LoadBMP("bullshit.bmp");
 	lies_surface = SDL_LoadBMP("lies.bmp");
@@ -53,6 +58,7 @@ int main(int argc, char* argv[]) {
 	bool shoot = false;
 	int i = 0;
 	int counter = 0;
+	int animations = 0;
 
 	while(quit == false) //SCREEN COLORS AND RECTANGLE
 	{
@@ -61,9 +67,9 @@ int main(int argc, char* argv[]) {
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, bullshit_texture, NULL, &background_1);
 		SDL_RenderCopy(renderer, bullshit_texture, NULL, &background_2);
-		SDL_RenderCopy(renderer, todd_texture, NULL, &rectangle);		
+		SDL_RenderCopy(renderer, todd_texture, &unicorn, &rectangle);		
 		for (i = 0; i < MAX_BULLETS; ++i) {
-			SDL_RenderCopy(renderer, lies_texture, NULL, &bullet[i]);		
+			SDL_RenderCopy(renderer, todd_texture, &shot, &bullet[i]);		
 			bullet[i].x++;
 		}	
 	
@@ -72,7 +78,7 @@ int main(int argc, char* argv[]) {
 		if (shoot) {
 			
 			bullet[counter].x = rectangle.x + 50;
-			bullet[counter].y = rectangle.y + 120;
+			bullet[counter].y = rectangle.y + 50;
 			shoot = false;
 		}
 
@@ -103,10 +109,14 @@ int main(int argc, char* argv[]) {
 							quit = true;
 							break;
 						case(SDLK_w):
-							rectangle.y -= 5;							
+							rectangle.y -= 5;		
+							unicorn.x = 845;
+							unicorn.y = 750;
 							break;
 						case(SDLK_s):
 							rectangle.y += 5;
+							unicorn.x = 1720;
+							unicorn.y = 740;
 							break;
 						case(SDLK_a):
 							rectangle.x -= 5;
@@ -116,6 +126,7 @@ int main(int argc, char* argv[]) {
 							break;
 						case(SDLK_SPACE):
 							++counter;
+							shot.y = 1680;
 							if (counter == MAX_BULLETS) {
 								counter = 0;
 							}							
@@ -135,6 +146,7 @@ int main(int argc, char* argv[]) {
 	SDL_DestroyTexture(todd_texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	IMG_Quit();
 	SDL_Quit();
 
 	return(EXIT_SUCCESS);
